f3c1f4c9e3616b19381f254859d432fe6564fd7e
David Elentok <3david@gmail.com>
2026-03-25 22:57:38 +0200

    tmux-targets: extract styles

--- internal/tmuxtargets/model.go
+++ internal/tmuxtargets/model.go
@@ -13,22 +13,6 @@ import (
 	"github.com/elentok/blf/internal/platform"
 )
 
-var (
-	baseStyle           = lipgloss.NewStyle().Foreground(lipgloss.Color(paletteText))
-	targetStyle         = lipgloss.NewStyle().Foreground(lipgloss.Color(palettePeach))
-	selectedStyle       = lipgloss.NewStyle().Foreground(lipgloss.Color(paletteBase)).Background(lipgloss.Color(palettePeach))
-	searchTargetStyle   = lipgloss.NewStyle().Foreground(lipgloss.Color(paletteBase)).Background(lipgloss.Color(paletteGreen))
-	searchSelectedStyle = lipgloss.NewStyle().Bold(true).Foreground(lipgloss.Color(paletteBase)).Background(lipgloss.Color(paletteGreen))
-	searchBoxStyle      = lipgloss.NewStyle().Border(lipgloss.RoundedBorder()).BorderForeground(lipgloss.Color(paletteGreen)).Foreground(lipgloss.Color(paletteGreen)).Width(38)
-)
-
-const (
-	paletteBase  = "#1e1e2e"
-	paletteText  = "#a6adc8"
-	palettePeach = "#fab387"
-	paletteGreen = "#a6e3a1"
-)
-
 type model struct {
 	lines        []string
 	targets      []target
